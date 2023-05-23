5838b3ed068051b426226f8af20c3e45ac701b15
Alex-Fan777 <132428764+Alex-Fan777@users.noreply.github.com>
2023-05-23 16:56:09 +1000

    Update 2023-05-23-fastai.mk

--- _posts/2023-05-23-fastai.mk
+++ _posts/2023-05-23-fastai.mk
@@ -5,9 +5,7 @@ variance in each run, you can use hyperparameter adjustment to preserve the dist
 Therefore, this algorithm requires a lot of time and space to calculate
 
 How can we understand it quickly?
-T-SNE is mainly used for dimensionality reduction of data and has a wide range of applications. For simplicity, the following is an example of t-SNE reducing 2D 
-data to one-dimensional.
-https://img-blog.csdnimg.cn/2019042920433744.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3hpZXNoYW5neGlu,size_16,color_FFFFFF,t_70
+T-SNE is mainly used for dimensionality reduction of data and has a wide range of applications. 
 
 
 
